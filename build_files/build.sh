5d7e23fba4969b54b1d629b83bf1e93a25e64083
John Mccaulley <john.mccaulley@outlook.com>
2026-09-20 02:35:59 -0400

    adjusting cp

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,7 +4,7 @@ set -ouex pipefail
 
 # Copy the contents of system_files/ of the git repo to /
 cp -avf "/ctx/system_files"/. /
-cp -avf "/ctx/lenovo/bin"/. /lib/firmware/
+cp -avf ../lenovo/bin/. /lib/firmware/
 
 ### Install packages
 
